c64bbb144eef6951bda85d1ac4d7845ce70ffd38
Yash Patel <ydpatel0309@gmail.com>
2023-03-07 19:37:38 +0530

    Update class EmerencyCallService.java

--- class EmerencyCallService.java
+++ class EmerencyCallService.java
@@ -1,6 +1,41 @@
-class EmerencyCallService
+import java.sql.Date;
+import java.time.LocalDate;
+
+interface EmerencyService
 {
-    class Ambluance{}
-    class Fire{}
-    class Police{}
+    void Ambulance();
+    void Police();
+    void Fire();
+}
+abstract class Service 
+{
+    String name;
+    long Mnumber;
+    String address;
+    
+    void Notifaction()
+    {
+        System.out.println(LocalDate.now());
+        System.out.println("Your call is now being Recorded");
+    }
+    void status()
+    {
+
+    }
+
+    class Police extends Service implements EmerencyService
+    {
+
+    }
+
+
+    class EmerencyCallService
+    {
+        public static void main()
+         {
+            
+        }
+
+    }
+
 }
